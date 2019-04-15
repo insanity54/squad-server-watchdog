ab7699f613e2a480b182793b5b386e1bcedc5a88
Chris Grimmett <chris@grimtech.net>
2019-04-15 06:26:32 +0000

    Create watchdog.js

---
+++ watchdog.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+const path = require('path');
+const squadServerPath = 'C:\servers\squad\SquadServer.exe';
+const squad = spawn(squadServerPath);
+
+squad.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+squad.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+squad.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
